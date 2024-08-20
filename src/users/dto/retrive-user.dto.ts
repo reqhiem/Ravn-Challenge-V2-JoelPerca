169bd18815a4dfddac960733883c5a74f75bf44d
Joel Perca <joelperca.vc@gmail.com>
2024-08-20 18:49:44 -0500

    feat-006: Swagger Documentation (#1)
    * feat: 🎸 Add User Model & Basic Auth Endpoint
    
    * feat: 🎸 Add Swagger tool

---
+++ src/users/dto/retrive-user.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RetrieveUserDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
