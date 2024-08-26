81a71fe479c6e9540f2cb71a85b1b2521affee61
Joel Perca <joelperca.vc@gmail.com>
2024-08-25 20:23:04 -0500

    fix: 🐛 Typo issues

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,5 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsStrongPassword, MinLength } from 'class-validator';
+
+class UserDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  firstName: string;
+
+  @ApiProperty()
+  lastName: string;
+}
 export class SignInDto {
   @ApiProperty()
   username: string;
@@ -9,4 +23,12 @@ export class SignInDto {
   @MinLength(6)
   @ApiProperty()
   password: string;
+}
+
+export class SignInResponseDto {
+  @ApiProperty()
+  accessToken: string;
+
+  @ApiProperty()
+  user: UserDto;
 }
